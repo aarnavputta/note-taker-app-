cd74ca4617ddebc480f6f6ef5c3dd77ebfc4a711
Aarnav Putta <puttaaarnav23@gmail.com>
2025-07-15 11:42:40 -0400

    added tailwind css reconfig

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      mono: ["var(--font-mono)", "ui-monospace", "SFMono-Regular", "Menlo", "Monaco", "Consolas", "Liberation Mono", "Courier New", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}; 
